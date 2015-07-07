0522e3d82b4319106967b620a4e3c948f27d5053
Martin Gonzalez <martin.c.glez.glez@gmail.com>
2015-07-07 18:40:47 -0500

    Fix patch for camera plugin not returning native URL
    Cherry picked from: 33ffb17c9780db93e44bc9daaa284d4809940a95

--- src/windows/CameraProxy.js
+++ src/windows/CameraProxy.js
@@ -165,11 +165,15 @@ module.exports = {
 
                             var storageFolder = Windows.Storage.ApplicationData.current.localFolder;
                             file.copyAsync(storageFolder, file.name, Windows.Storage.NameCollisionOption.replaceExisting).then(function (storageFile) {
-                                successCallback(URL.createObjectURL(storageFile));
+                                if(destinationType == Camera.DestinationType.NATIVE_URI) {
+                                    successCallback("ms-appdata:///local/" + storageFile.name);
+                                }
+                                else {
+                                    successCallback(URL.createObjectURL(storageFile));
+                                }
                             }, function () {
                                 errorCallback("Can't access localStorage folder.");
                             });
-
                         }
                     }
                     else {
